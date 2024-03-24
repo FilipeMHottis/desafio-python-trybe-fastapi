02ae3169e011911d736342dd38950004b6366415
Filipe Marques Hottis <lipehottis@gmail.com>
2024-03-24 13:39:37 -0300

    feat: New Controller Created

--- src/app/main.py
+++ src/app/main.py
@@ -1,16 +1,18 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from app.settings import get_settings
 
 from controller.music_controller import router as music_controller
+from controller.render_controller import router as render_controller
 
 settings = get_settings()
 
 app = FastAPI(title=settings.api_name)
-
-
-@app.get("/", tags=["root"])
-def online_api():
-    return {"message": "API is online"}
-
+app.mount(
+    "/static",
+    StaticFiles(directory="src/view/static"),
+    name="static",
+)
 
 app.include_router(music_controller, prefix="/musics", tags=["musics"])
+app.include_router(render_controller, tags=["render"])
